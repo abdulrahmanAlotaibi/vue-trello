532f9052e73a2753a5dab7e54d3b368dc61fc814
eldh97 <eldh97@outlook.com>
2020-08-01 16:56:26 +0300

    first commit

---
+++ src/views/Board.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="board">
+    <div class="flex flex flex-row items-start">
+      <div class="column" v-for="(column, index) of board.columns" :key="index">
+        <div class="flex items-center mb-2 font-bold">
+          {{ column.name }}
+        </div>
+        <div class="list-reset">
+          <div
+            class="task"
+            v-for="(task, index) of column.tasks"
+            :key="index"
+            @click="goToTask(task)"
+          >
+            <span class="w-full flex-no-shrink font-bold">
+              {{ task.name }}
+            </span>
+            <p
+              v-if="task.description"
+              class="w-full flex-no-shrink mt-1 text-sm"
+            >
+              {{ task.description }}
+            </p>
+          </div>
+
+          <input
+            type="text"
+            class="block p-2 w-full bg-transparent"
+            placeholder="+ Enter new task"
+            @keyup.enter="createTask($event, column.tasks)"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div v-if="isTaskOpen" class="task-bg" @click.self="close">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState(["board"]),
+    isTaskOpen() {
+      return this.$route.name === "task";
+    }
+  },
+  methods: {
+    goToTask(task) {
+      this.$router.push({ name: "task", params: { id: task.id } });
+    },
+    close() {
+      this.$router.push({ name: "board" });
+    },
+    createTask(evt, tasks) {
+      this.$store.commit("CREATE_TASK", {
+        tasks,
+        name: evt.target.value
+      });
+      evt.target.value = "";
+    }
+  }
+};
+</script>
+
+<style lang="css">
+.task {
+  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
+}
+
+.column {
+  @apply bg-grey-light p-2 mr-4 text-left shadow rounded;
+  min-width: 350px;
+}
+
+.board {
+  @apply p-4 bg-teal-dark h-full overflow-auto;
+}
+
+.task-bg {
+  @apply pin absolute;
+  background: rgba(0,0,0,0.5);
+}
+</style>
---
+++ src/views/Task.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="task-view">
+    <div class="flex flex-col flex-grow items-start justify-between px-4">
+      <input
+        type="text"
+        :value="task.name"
+        class="p-2 w-full mr-2 block text-xl font-bold"
+        @change="updateTaskProperty($event, 'name')"
+        @keyup.enter="updateTaskProperty($event, 'name')"
+      />
+      <textarea
+        :value="task.description"
+        class="relative bg-transparent px-2 border mt-2 h-64 border-none leading-normal"
+        @change="updateTaskProperty($event, 'description')"
+        @keyup.enter="updateTaskProperty($event, 'name')"
+      ></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["getTask"]),
+    task() {
+      return this.getTask(this.$route.params.id);
+    }
+  },
+  methods: {
+    updateTaskProperty(evt, key) {
+      this.$store.commit("UPDATE_TASK", {
+        task: this.task,
+        key,
+        value: evt.target.value
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.task-view {
+  @apply relative flex flex-row bg-white pin mx-4 m-32 mx-auto py-4 text-left rounded shadow;
+  max-width: 700px;
+}
+</style>
